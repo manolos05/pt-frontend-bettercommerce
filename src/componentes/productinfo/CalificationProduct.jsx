76f03e27cffea020b09a412fc37d27b6136fb987
Manuel Salazar <121062158+manolos05@users.noreply.github.com>
2023-11-15 23:26:52 -0300

    update: calificationProducts

--- src/componentes/productinfo/CalificationProduct.jsx
+++ src/componentes/productinfo/CalificationProduct.jsx
@@ -1,18 +1,23 @@
 import StarPurple500OutlinedIcon from '@mui/icons-material/StarPurple500Outlined';
-
+import RateReviewOutlinedIcon from '@mui/icons-material/RateReviewOutlined';
 
 export const CalificationProduct = () => {
 
     return (
         <>
-        <div className="mt-4" >
+        <div className="d-flex mt-4 align-items-center justify-content-start" >
             <div>
-            12
+             <p><strong style={{weight: '700'}}>12 </strong> valoracions de clientes <StarPurple500OutlinedIcon className="star" /> <strong style={{color: '#4A4A6A'}}>4.8 </strong> </p>
             </div>
+            <div className="ms-4 ps-4 write-comment">
+            
+              <p style={{weight: '100'}}> <RateReviewOutlinedIcon /> <strong>Escribir cometario</strong> </p>
+            </div>
+            
 
         </div>
 
-        <p> <strong></strong> valoracions de clientes <StarPurple500OutlinedIcon className="star" /> 4.8 </p>
+        
         
         
         
