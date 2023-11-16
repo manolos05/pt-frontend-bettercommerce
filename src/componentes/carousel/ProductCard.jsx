85043180202f32de984e761885d63edf51f3ab4f
Manuel Salazar <121062158+manolos05@users.noreply.github.com>
2023-11-16 08:55:22 -0300

    iniciando fecth productos

--- src/componentes/carousel/ProductCard.jsx
+++ src/componentes/carousel/ProductCard.jsx
@@ -1,23 +1,23 @@
 import img from './carouselImg.png';
 import '../../style/product-card.css';
 
-export const ProductCard = () => {
+export const ProductCard = ({brand, product_name,  price, discount}) => {
   return (
     <div className='card text-start border-0'>
       {' '}
       <img src={img} alt='' />
       <small style={{ fontSize: '12px' }} className='mt-3 fw-light'>
-        Brand
+        {brand}
       </small>
-      <p>Shaker Brand 400cc</p>
+      <p>{product_name}</p>
       <div
         style={{
           display: 'flex',
           alignItems: 'center',
         }}
       >
-        <h2 className='d-inline text-main-color mr-1'>$7.990</h2>{' '}
-        <span className='text-decoration-line-through '>$9.990</span>
+        <h2 className='d-inline text-main-color mr-1'>{discount}</h2>{' '}
+        <span className='text-decoration-line-through '>{price}</span>
       </div>
       <div className='free-shiping '>ENVIO GRATIS STGO</div>
     </div>
