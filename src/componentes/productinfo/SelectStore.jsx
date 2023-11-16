aaf86cb5e697863468747023ea104ced24405076
Manuel Salazar <121062158+manolos05@users.noreply.github.com>
2023-11-15 21:50:36 -0300

    2do y 3er fecht comunas y regiones para retiro en tienda TERMINADO

--- src/componentes/productinfo/SelectStore.jsx
+++ src/componentes/productinfo/SelectStore.jsx
@@ -6,37 +6,48 @@ export const SelectStore = () => {
     const [ regiones, setRegiones ] = useState([]);
     const [ comunas, setComunas ] = useState([]);
 
+    const [ idComuna, setIdComuna ] = useState("")
+
+    const getComuna = async () => {
+        const res = await fetch(`http://127.0.0.1:3333/comunas?txtBuscar=${idComuna}`);
+        const data = await res.json();
+        setComunas(data);
+    }
+
     useEffect(() => {
 
         try {
-            const getFlavorProtien = async () => {
+            const getRegiones = async () => {
               const res = await fetch("http://127.0.0.1:3333/regions/");
               const data = await res.json();
               setRegiones(data);
             };
-            getFlavorProtien();
+            getRegiones();
           } catch (error) {
             console.log("error", error);
           }
-          try {
-            const getFlavorProtien = async () => {
-              const res = await fetch("http://127.0.0.1:3333/comunas/");
-              const data = await res.json();
-              setComunas(data);
-            };
-            getFlavorProtien();
-          } catch (error) {
-            console.log("error", error);
-          }
+          
     
     }, [])
 
-   
+    useEffect(() => {
+        if (idComuna !== "") {
+            try {
+                getComuna()
+            }
+            catch(error){
+                console.log(error)
+            }
+           
+        }
+       
+    }, [idComuna])
+
 
     return (
         <div className="d-flex ">
 
-            <select className="form-select" aria-label="Default select example" style={{borderColor: '#EB00D3', borderRadius: '30px', fontSize: '10px'}}>
+            <select className="form-select" aria-label="Default select example" style={{borderColor: '#EB00D3', borderRadius: '30px', fontSize: '10px'}} onChange={(e) => setIdComuna(e.target.value) }>
                 <option selected>SELECCIONA TU REGION</option>
                 {regiones.length !== 0 ? (
                         regiones.map(({ region, identificador}, id ) => (
@@ -49,11 +60,11 @@ export const SelectStore = () => {
             <select className="form-select ms-2" aria-label="Default select example" style={{borderColor: '#EB00D3', borderRadius: '30px', fontSize: '10px'}}>
                 <option selected>SELECCIONA TU COMUNA</option>
                 {comunas.length !== 0 ? (
-                        regiones.map(({ comuna, identificador}, id ) => (
+                        comunas.map(({ comuna, identificador}, id ) => (
                             <option value={identificador} className="text-dark" key={id}>{comuna}</option>
                         ))
                     ) : (
-                        <option></option>
+                        <option>Selecciona un Región</option>
                     )} 
             
             </select>
